bf58031ddfcae6881fabf30bd6afaa0fa787c81d
Eddie Zaneski <eddiezane@gmail.com>
2014-11-04 11:10:58 -0500

    Do a lil regex sumsum

--- app.rb
+++ app.rb
@@ -5,7 +5,15 @@ require 'uri'
 
 post '/' do
   hc = HipChat::Client.new(ENV['HIPCHAT_TOKEN'], api_version: 'v2')
-  question = URI.extract(params['text'])[7]
+  links = URI.extract(params['text'])
+
+  question = nil
+  links.each do |l|
+    next unless l =~ /http:\/\/stackoverflow\.com\/questions\/.*/
+    question = l
+    break
+  end
+
   hc['Dev Relations'].send('Stacky', "New Stack Overflow question <a href='#{question}'>#{question}</a>")
   200
 end
