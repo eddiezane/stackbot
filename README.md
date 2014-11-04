28f5129682b9d2c689433724b88f2f23482439d5
Eddie Zaneski <eddiezane@gmail.com>
2014-11-04 10:49:02 -0500

    Add README

---
+++ README.md
@@ -0,0 +1,5 @@
+Stack HipChat Bot
+=================
+
+
+Posts new Stack Overflow questions into HipChat using SO email notifications and the SendGrid inbound parse API.
--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'uri'
 
 post '/' do
   hc = HipChat::Client.new(ENV['HIPCHAT_TOKEN'], api_version: 'v2')
-
-  hc['Dev Relations'].send('Stacky', "New Stack Overflow question <a href='#{URI.extract(params['text'])[7]}'>#{URI.extract(params['text'])[7]}</a>")
+  question = URI.extract(params['text'])[7]
+  hc['Dev Relations'].send('Stacky', "New Stack Overflow question <a href='#{question}'>#{question}</a>")
   200
 end
