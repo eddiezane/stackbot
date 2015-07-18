0ec8d6e88600aa4362f96606f63a107d5debefb0
Eddie Zaneski <eddiezane@gmail.com>
2015-07-18 19:52:11 -0400

    Add heroku buttom

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-Stack HipChat Bot
+StackBot
 =================
 
+Posts new Stack Overflow questions into HipChat using SO email notifications and the SendGrid [Inbound Parse Webhook](https://sendgrid.com/docs/API_Reference/Webhooks/parse.html).
 
-Posts new Stack Overflow questions into HipChat using SO email notifications and the SendGrid inbound parse API.
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+## Usage
+
+Deploy to Heroku using the button above. Make sure to provide the required keys.
+
+Add a new entry to your SendGrid [Inbound Parse Settings](https://app.sendgrid.com/settings/parse). You can either provide your own domain or use `anything-thats-not-taken.bymail.in`. Point it at your Heroku app.
+
+
+
+Visit [http://stackexchange.com/filters/new](http://stackexchange.com/filters/new) and create a new filter with your tag. For email address, use `anything@the-domain-you-used-above.bymail.in`.
+
+Open your apps logs
+
+## ENV Vars
+
+```
+HIPCHAT_TOKEN=A HipChat v2 API Token
+HIPCHAT_ROOMS=A comma separated list of HipChat rooms or ID's. e.g. 'Support Room,Community'
+```
--- app.rb
+++ app.rb
@@ -3,25 +3,30 @@ require 'sinatra'
 require 'thin'
 require 'uri'
 
+CONFIRMATION = /http:\/\/stackexchange\.com\/filters\/email-confirm\/\w*/
+QUESTION = /http:\/\/stackoverflow\.com\/questions\/.*/
+
 post '/' do
-  hc = HipChat::Client.new(ENV['HIPCHAT_TOKEN'], api_version: 'v2')
   links = URI.extract(params['text'])
 
-  questions = []
   links.each do |link|
     case link
-    when /http:\/\/stackexchange\.com\/filters\/email-confirm\/.*/
-      puts "Confirmation: #{link}"
-    when /http:\/\/stackoverflow\.com\/questions\/.*/
-      questions << link
-    end
-  end
-
-  questions.each do |question|
-    ENV['HIPCHAT_ROOMS'].split(',').each do |r|
-      hc[r].send('StackBot', "New Stack Overflow question <a href='#{question}'>#{question}</a>")
+    when CONFIRMATION
+      post_message("StackBot confirmation <a href='#{link.scan(CONFIRMATION)[0]}'>link</a>")
+    when QUESTION
+      post_message("New Stack Overflow question <a href='#{question}'>#{question}</a>")
     end
   end
 
   200
+end
+
+def post_message(message)
+  ENV['HIPCHAT_ROOMS'].split(',').each do |r|
+    hc[r].send('StackBot', message)
+  end
+end
+
+def hc
+  @hc ||= HipChat::Client.new(ENV['HIPCHAT_TOKEN'], api_version: 'v2')
 end
